2d41796987bce8bdf22e9c978a346dcb70fbf509
Cody Malick <cody.malick@gmail.com>
2015-08-17 13:19:00 -0700

    projects with angular

--- app.js
+++ app.js
@@ -7,6 +7,27 @@
 
     app.controller('PageController', function() {
       console.print("This is working");
+
       // this.products = gems;
+    });
+    app.controller('ProjectController', function() {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://api.github.com/users/codymalick/repos", false);
+      xhr.send();
+      var data = xhr.responseText;
+      console.log(data);
+
+      var items = [];
+      angular.forEach(data, function(projects) {
+        angular.forEach(projects, function(project) {
+          items.push(project)
+        });
+      });
+
+      console.log(items);
+      // this.projects = items;
+      // angular.forEach(this.projects, function(projects) {
+      //   console.log(projects.name);
+      // });
     });
 })();
