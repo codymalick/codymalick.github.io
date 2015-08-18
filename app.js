d34c06aac45e9a17c19d0498ba9f58f004c6ea6a
Cody Malick <cody.malick@gmail.com>
2015-08-18 15:28:35 -0700

    Fixed gitignore, added contributors field to projects

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
     });
     app.controller('ProjectController', function() {
       var xhr = new XMLHttpRequest();
+
       xhr.open("GET", "https://api.github.com/users/codymalick/repos", false);
       xhr.send();
 
@@ -16,11 +17,21 @@
 
       var items = [];
       angular.forEach(data, function(project) {
-          var proj_data = {
-            name : project.name,
-            url : project.html_url,
-            desc : project.description
-          }
+
+        // var xhr0 = new XMLHttpRequest();
+        // xhr0.open("GET", "https://api.github.com/repos/codymalick/codymalick.github.io/stats/contributors");
+        // xhr0.send();
+        // console.log(xhr0.responseText);
+        // console.log(project.name);
+        // var contributors = JSON.parse(xhr0.responseText);
+        // console.log(contributors);
+
+        var proj_data = {
+          name : project.name,
+          url : project.html_url,
+          desc : project.description,
+          stars : project.stargazers_count
+        }
           items.push(proj_data);
       });
 
