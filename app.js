60905b3f8f1f75b8b68a11c86b268cf5ddfabd38
Cody Malick <cody.malick@gmail.com>
2015-08-19 13:51:23 -0700

    fixed broken javascript

--- app.js
+++ app.js
@@ -7,29 +7,36 @@
     });
     app.controller('ProjectController', function() {
       var xhr = new XMLHttpRequest();
-      var items = [];
+      
 
-      xhr.onreadystatechange=function() {
-        if (xhr.readyState==4 && xhr.status==200) {
-          var data = JSON.parse(xhr.responseText);
-          console.log(data);
 
-          angular.forEach(data, function(project) {
-            var proj_data = {
-              name : project.name,
-              url : project.html_url,
-              desc : project.description,
-              stars : project.stargazers_count
-            }
-              items.push(proj_data);
-          });
-        }
-      }
-
-      xhr.open("GET", "https://api.github.com/users/codymalick/repos", true);
+      xhr.open("GET", "https://api.github.com/users/codymalick/repos", false);
       xhr.send();
 
       //Gotta remember to parse the json
+      var data = JSON.parse(xhr.responseText);
+      console.log(data);
+
+      var items = [];
+      angular.forEach(data, function(project) {
+
+        // var xhr0 = new XMLHttpRequest();
+        // xhr0.open("GET", "https://api.github.com/repos/codymalick/codymalick.github.io/stats/contributors");
+        // xhr0.send();
+        // console.log(xhr0.responseText);
+        // console.log(project.name);
+        // var contributors = JSON.parse(xhr0.responseText);
+        // console.log(contributors);
+
+        var proj_data = {
+          name : project.name,
+          url : project.html_url,
+          desc : project.description,
+          stars : project.stargazers_count
+        }
+          items.push(proj_data);
+      });
+
       console.log(items);
       this.projects = items;
 
